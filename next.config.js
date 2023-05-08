8de8bfaaec0086eb4c26bed2b6b39cb9f0341195
Abhinay Singh <abhinaysingh.acnt@gmail.com>
2023-05-08 18:53:32 +0530

    next auth added

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
   images: {
     domains: ["media.kijiji.ca"],
   },
+  env: {
+    GOOGLE_CLIENT_ID : "630576064420-368h7n73q1t07uhvhkdl3sr7k0910m7j.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET : "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",
+    FACEBOOK_CLIENT_ID : "1373170750141686",
+    FACEBOOK_CLIENT_SECRET : "b66f605a1b18487b42773e4056f344d2",
+    NEXT_AUTH_SECRET : "c53180cfa41b0970fd3b2e9eb124fa2e",
+  },
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import { FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, NEXT_AUTH_SECRET } from "@/Environment";
 
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
@@ -7,13 +6,13 @@ import FacebookProvider from "next-auth/providers/facebook";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     FacebookProvider({
-      clientId: FACEBOOK_CLIENT_ID,
-      clientSecret: FACEBOOK_CLIENT_SECRET,
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
-  secret: NEXT_AUTH_SECRET,
+  secret: process.env.NEXT_AUTH_SECRET,
 });
