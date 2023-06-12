7dd39e2e55eeee7924b5d8e5056f921162946f97
Abhinay Singh <abhinaysingh.acnt@gmail.com>
2023-06-12 15:03:27 +0530

    welcome popup content added

---
+++ components/Dialog/Dialogs/WelcomeDialog.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const WelcomeDialog = () => {
+    return (
+        <div className='welcome-popup-dialog'>
+            <h2>Welcome to Adzone!</h2>
+            <p>This website is currently in <span>demo mode</span>, and all the listings you see here are samples for testing purposes only. Our intention here to provide you with a realistic experience of a production website.</p>
+            <p>Please note that the information provided in these sample listings, including website links, has been curated from various sources. However, it&apos;s important to emphasize that these listings are for demonstration purposes only and do not represent real products, services, or websites.</p>
+            <p>We want to assure you that our intention is not to copy or steal content from other websites. Instead, we have carefully selected and modified data to create a simulated environment that resembles the content typically found on classified websites. We respect the intellectual property rights of other websites.</p>
+            <p>If you are a representative of any of the original websites and have concerns, please reach out to us through the provided contact channels, and we will promptly address them.</p>
+            <p>Adzone provides a platform for users to explore and interact with these sample listings, giving you a glimpse of how a production website would function. However, please keep in mind that no transactions or actual purchases can be made, and the linked websites are not operational.</p>
+            <p>Thank you for visiting Adzone! If you have any questions or need assistance, please reach our support team.</p>
+            <p>Enjoy your experience!</p>
+        </div>
+    )
+}
+
+export default WelcomeDialog
